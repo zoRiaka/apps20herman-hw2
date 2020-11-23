b88807cad88eff886ff1e7822aedba93770ab382
zoriankaH <z.herman@ucu.edu.ua>
2020-11-23 18:24:33 +0200

    adding classes

--- src/main/java/ua/edu/ucu/collections/Queue.java
+++ src/main/java/ua/edu/ucu/collections/Queue.java
@@ -1,5 +1,33 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 public class Queue {
-    
+    private ImmutableLinkedList queue;
+
+    public Queue(ImmutableLinkedList queue) {
+        this.queue = queue;
+    }
+
+    public void enqueue(Object e)
+    {
+        queue = queue.addLast(e);
+    }
+
+    public Object dequeue()
+    {
+        Object el = queue.getFirst();
+        queue = queue.removeFirst();
+        return el;
+    }
+
+    public Object peek()
+    {
+        return queue.getFirst();
+    }
+
+    public ImmutableLinkedList getQueue() {
+        return queue;
+    }
+
 }
--- src/main/java/ua/edu/ucu/collections/Stack.java
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -1,5 +1,28 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 public class Stack {
-    
+    private ImmutableLinkedList stack;
+    public Stack(ImmutableLinkedList stack) {
+        this.stack = stack;
+    }
+
+    public void push(Object el) {
+        stack.addFirst(el);
+    }
+
+    public Object pop() {
+        Object el = stack.getFirst();
+        stack.removeFirst();
+        return el;
+    }
+
+    public Object peek() {
+        return  stack.getFirst();
+    }
+
+    public ImmutableLinkedList getStack() {
+        return stack;
+    }
 }
